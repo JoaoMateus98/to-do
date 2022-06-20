42882802d9fb76b3149214f812439e7f6469a5f0
JoaoMateus98 <joaobsu98@gmail.com>
2022-06-20 17:15:56 -0400

    added a dev surver and other necessary files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    client: {
+      reconnect: true
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To-Do',
+    }),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+    ]
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
